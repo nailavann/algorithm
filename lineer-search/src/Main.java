334f511c715380a2d0252c30e7c34d737c4c91bc
nailavann <nailavann@hotmail.com>
2023-11-23 15:05:30 +0300

    Initial commit

---
+++ lineer-search/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+           Lineer Search
+           Time Complexity O(N)
+        */
+
+        int[] array = {23, 0, 1, 4, 6, 85463, 45, 6, 73, 234, 6, 2, 21};
+        int search = 84;
+        int index = lineerSearch(array, search);
+        if (index != -1) {
+            System.out.println("Index: " + index);
+        } else {
+            System.out.println("Not in the array...");
+        }
+
+    }
+
+    private static int lineerSearch(int[] array, int search) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == search) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ selection-sort/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Selection Sort Algorithm
+        Time Complexity O(N^2)
+        */
+
+        int[] unSortedArray = {23,0, 1, 4, 6, 85463, 45, 6, 73, 234, 6, 2, 21};
+        System.out.println("Sorting operation...");
+
+        int[] sortedArray = selectionSort(unSortedArray);
+
+        for (int i : sortedArray) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+        return array;
+    }
+}
